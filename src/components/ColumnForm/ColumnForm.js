c32ed7e76f26af9d0435009996455163b23b3279
kris.kensy <kriskensy@proton.me>
2025-08-21 21:17:03 +0200

    Add action ADD_COLUMN to reducer in store. Add dispatch to ColumnForm

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -2,30 +2,30 @@ import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
 import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
+import { useDispatch } from 'react-redux';
 
 const ColumnForm = props => {
 
-    const [title, setTitle] = useState('');
-    const [icon, setIcon] = useState('');
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [icon, setIcon] = useState('');
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        props.action({ title: title, icon: icon});
-        setTitle('');
-        setIcon('');
-    }
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: 'ADD_COLUMN', newColumn: { title, icon } });
+    setTitle('');
+    setIcon('');
+  }
 
-    return (
-        <form className={styles.columnForm} onSubmit={handleSubmit}>
-            <label className={styles.label}>Title:</label>
-            {/* <input type='text' value={title} onChange={e => setTitle(e.target.value)}/> */}
-            <TextInput value={title} onChange={e => setTitle(e.target.value)}/>
-            <label className={styles.label}>Icon:</label>
-            {/* <input type='text' value={icon} onChange={e => setIcon(e.target.value)}/> */}
-            <TextInput value={icon} onChange={e => setIcon(e.target.value)}/>
-            <Button>Add column</Button>
-        </form>
-    );
+  return (
+    <form className={styles.columnForm} onSubmit={handleSubmit}>
+      <label className={styles.label}>Title:</label>
+      <TextInput value={title} onChange={e => setTitle(e.target.value)}/>
+      <label className={styles.label}>Icon:</label>
+      <TextInput value={icon} onChange={e => setIcon(e.target.value)}/>
+      <Button>Add column</Button>
+    </form>
+  );
 };
 
 export default ColumnForm;
