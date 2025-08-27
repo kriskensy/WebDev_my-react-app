dce1a73b80181c985638b17df2fe3485e15d50e9
kris.kensy <kriskensy@proton.me>
2025-08-27 19:53:02 +0200

    Add getFilteredCards selector to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 import {createStore} from 'redux';
 import initialState from './initialState';
 
+//selectors
+export const getFilteredCards = (state, columnId) => state.cards.filter(card => card.columnId === columnId && card.title.toLowerCase().includes(state.searchString.toLowerCase()));
+
 const reducer = (state, action) => {
   if(action.type === 'ADD_COLUMN') return { ...state, columns: [...state.columns, action.newColumn]}
   if(action.type === 'UPDATE_SEARCHSTRING') return { ...state, searchString: action.payload }
