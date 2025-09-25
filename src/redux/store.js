520d2438cbbf0738575e842002d5ee092f0e290c
kris.kensy <kriskensy@proton.me>
2025-09-25 10:24:58 +0200

    Add combineReducers function

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import {createStore} from 'redux';
+import {createStore, combineReducers} from 'redux';
 import initialState from './initialState';
 import { strContains } from '../utils/strContains';
 import { cardMaxId } from '../utils/cardMaxId';
@@ -70,16 +70,14 @@ const searchStringReducer = (statePart = '', action) => {
   }
 }
 
-const reducer = (state, action) => {
-  const newState = {
-    lists: listsReducer(state.lists, action),
-    columns: columnsReducer(state.columns, action),
-    cards: cardsReducer(state.cards, action),
-    searchString: searchStringReducer(state.searchString, action)
-  };
-
-  return newState;
+const subreducers = {
+  lists: listsReducer,
+  columns: columnsReducer,
+  cards: cardsReducer,
+  searchString: searchStringReducer
 }
+
+const reducer = combineReducers(subreducers);
 
 const store = createStore(
   reducer,
